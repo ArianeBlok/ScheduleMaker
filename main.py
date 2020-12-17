017f5d209fc002ee311a607192fd887ee08be531
Ariane Blok <d.a.n.e.blok@students.uu.nl>
2020-12-17 23:19:30 +0100

    added small explaination

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+# This will be the main programm:
+# importing the cvs file, 
+# making a graph, 
+# running the allogaritm 
+# and generating an output file with a scedule
+
 import pandas as pd #for datastructure
 import networkx as nx #graph manipulation
 
